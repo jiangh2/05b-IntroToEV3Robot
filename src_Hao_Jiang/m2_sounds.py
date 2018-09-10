efb130e919947bd55b5c97757b83d5e0e188c46d
jiangh2 <Ydong6688@>
2018-09-10 17:09:32 -0400

    sddfd

--- src_Hao_Jiang/m2_sounds.py
+++ src_Hao_Jiang/m2_sounds.py
@@ -11,8 +11,11 @@ Authors: Dave Fisher, David Mutchler, Vibha Alangar,
 #   Using the DOT trick, add code to  make_sounds  to make the robot
 #   make sounds in various ways:  Beep, Tone, Speech, Song.
 # ------------------------------------------------------------------------------
-
+import rosebotics as rb
 def main():
+
+
+
     fun_song = [(392, 350, 100), (392, 350, 100), (392, 350, 100),
                 (311.1, 250, 100), (466.2, 25, 100), (392, 350, 100),
                 (311.1, 250, 100), (466.2, 25, 100), (392, 700, 100),
@@ -36,5 +39,8 @@ def main():
                 (311.13, 250, 100), (466.16, 25, 100), (392.00, 300, 150),
                 (311.13, 250, 100), (466.16, 25, 100), (392, 700)]
 
+
+    song = rb.Tone(3000, 100)
+    song.play()
 
 main()
